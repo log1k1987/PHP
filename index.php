28ea07e0bc9c6d728080fb4d529799579a7cf225
log1k <log1k@mail.ru>
2019-02-16 12:58:58 +0300

    task2

--- index.php
+++ index.php
@@ -9,3 +9,4 @@ $for_task1 = [
 
 task1($for_task1, true);
 
+task2('*', 2, 6, 3.2);
--- src/functions.php
+++ src/functions.php
@@ -12,3 +12,33 @@ function task1($massive = [], $switcher = false)
         echo "<b>$massive[$i]</b>";
     }
 }
+
+function task2()
+{
+    if (func_num_args() > 1 && is_string(func_get_arg(0))) {
+        $x = func_get_arg(0);
+        $result = func_get_arg(1);
+
+        for ($i = 2; $i < func_num_args(); $i++) {
+            switch ($x) {
+                case '+':
+                    $result += func_get_arg($i);
+                    break;
+                case '-':
+                    $result -= func_get_arg($i);
+                    break;
+                case '*':
+                    $result *= func_get_arg($i);
+                    break;
+                case '/':
+                    $result /= func_get_arg($i);
+                    break;
+                default:
+                    $result = 'Передан неверный первый аргумент';
+            }
+        }
+    } else {
+        $result = 'Мало данных';
+    }
+    echo $result;
+}
