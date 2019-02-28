d57ee26559ecb2c6dfe2325fa698694acd5de39f
log1k <log1k@mail.ru>
2019-02-28 21:12:36 +0300

    dump

--- burgers.sql
+++ burgers.sql
@@ -1,115 +1,79 @@
--- phpMyAdmin SQL Dump
--- version 4.8.3
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 8.0.12, for Win64 (x86_64)
 --
--- Хост: 127.0.0.1:3306
--- Время создания: Фев 28 2019 г., 21:02
--- Версия сервера: 5.6.41
--- Версия PHP: 7.2.10
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: burgers
+-- ------------------------------------------------------
+-- Server version	5.6.41
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+ SET NAMES utf8mb4 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- База данных: `burgers`
---
-CREATE DATABASE IF NOT EXISTS `burgers` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `burgers`;
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `orders`
+-- Table structure for table `orders`
 --
 
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `orders` (
-  `order_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `address` varchar(256) DEFAULT NULL,
-  `comment` text
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `comment` text,
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Дамп данных таблицы `orders`
+-- Dumping data for table `orders`
 --
 
-INSERT INTO `orders` (`order_id`, `user_id`, `address`, `comment`) VALUES
-(43, 16, 'Koryavya 4 1 5 11', 'utututu daaaaaaaaaaaaaaaaaaaaaaa'),
-(44, 16, 'Koryavya 4 1 5 11', 'utututu daaaaaaaaaaaaaaaaaaaaaaa'),
-(45, 17, 'Novokosinskaia 2 2 2 2', '2222222222222222222222222'),
-(46, 18, 'Novokosinskaia 2 2 2 2', '2222222222222222222222222'),
-(47, 19, 'Novokosinskaia 2 2 2 2', '2222222222222222222222222'),
-(48, 20, 'Марканая 9 22 6 4', 'ddddddddddd'),
-(49, 20, 'Марканая 9 22 6 4', 'dddddddddddd'),
-(50, 20, 'Марканая 9 22 6 4', 'ssssssssssss'),
-(51, 20, 'Марканая 9 22 6 4', 'dddddddddddd'),
-(52, 20, 'Марканая 9 22 6 4', 'sssssssssssss');
-
--- --------------------------------------------------------
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT INTO `orders` VALUES (43,16,'Koryavya 4 1 5 11','utututu daaaaaaaaaaaaaaaaaaaaaaa'),(44,16,'Koryavya 4 1 5 11','utututu daaaaaaaaaaaaaaaaaaaaaaa'),(45,17,'Novokosinskaia 2 2 2 2','2222222222222222222222222'),(46,18,'Novokosinskaia 2 2 2 2','2222222222222222222222222'),(47,19,'Novokosinskaia 2 2 2 2','2222222222222222222222222'),(48,20,'Марканая 9 22 6 4','ddddddddddd'),(49,20,'Марканая 9 22 6 4','dddddddddddd'),(50,20,'Марканая 9 22 6 4','ssssssssssss'),(51,20,'Марканая 9 22 6 4','dddddddddddd'),(52,20,'Марканая 9 22 6 4','sssssssssssss');
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Структура таблицы `users`
+-- Table structure for table `users`
 --
 
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(40) NOT NULL,
   `name` varchar(40) DEFAULT NULL,
-  `phone` varchar(40) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `phone` varchar(40) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Дамп данных таблицы `users`
+-- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `email`, `name`, `phone`) VALUES
-(16, 'mishutka@mail.ru', 'Пинкор', '+7 (999) 999 99 99'),
-(17, 'vorotova1988@yandex.ru', 'Николай', '+7 (444) 444 44 4_'),
-(18, 'voraaaotova1988@yandex.ru', 'Николай', '+7 (444) 444 44 4_'),
-(19, 'saraaaotova1988@yandex.ru', 'Николай', '+7 (444) 444 44 4_'),
-(20, 'zog1k@mail.ru', 'иВан', '+7 (666) 666 66 66');
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (16,'mishutka@mail.ru','Пинкор','+7 (999) 999 99 99'),(17,'vorotova1988@yandex.ru','Николай','+7 (444) 444 44 4_'),(18,'voraaaotova1988@yandex.ru','Николай','+7 (444) 444 44 4_'),(19,'saraaaotova1988@yandex.ru','Николай','+7 (444) 444 44 4_'),(20,'zog1k@mail.ru','иВан','+7 (666) 666 66 66');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Индексы сохранённых таблиц
---
-
---
--- Индексы таблицы `orders`
---
-ALTER TABLE `orders`
-  ADD PRIMARY KEY (`order_id`);
-
---
--- Индексы таблицы `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT для сохранённых таблиц
---
-
---
--- AUTO_INCREMENT для таблицы `orders`
---
-ALTER TABLE `orders`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
-
---
--- AUTO_INCREMENT для таблицы `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-02-28 21:10:57
